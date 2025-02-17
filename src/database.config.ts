32a5c45d2adcc6797792f2dd8d16972b5d4c896d
Michael Busbee <busbee.michael101@gmail.com>
2025-02-17 11:29:44 -0500

    Setup server side to database connection

---
+++ src/database.config.ts
@@ -0,0 +1,15 @@
+import { ConfigModule } from '@nestjs/config';
+import { DataSourceOptions } from 'typeorm';
+
+ConfigModule.forRoot();
+
+export const databaseConfig: DataSourceOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT || 3306),
+  username: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'loggon_db',
+  synchronize: true,
+  entities: ['src/entities/*.ts'],
+};
