c03a7090ee9c17f974c95b74ca6e47f50153dbc7
Michael Busbee <busbee.michael101@gmail.com>
2025-02-25 13:37:31 -0500

    Added User Dto and updated user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Inject, Injectable, Param } from '@nestjs/common';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './Data-Transfer-Objects/CreateUserDto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @Inject(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    const { email, password } = createUserDto;
+
+    const regex = '';
+  }
+}
