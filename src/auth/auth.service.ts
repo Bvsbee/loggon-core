a25097e285ec63b84372b9dddd167b0b279d70ab
E-Roon <ecfrancis45@gmail.com>
2025-04-08 12:32:58 -0400

    Current Code for the backend

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,52 +9,6 @@ import { User } from '../user/user.entity';
 
 @Injectable()
 export class AuthService {
-<<<<<<< HEAD
-    constructor(
-        @InjectRepository(User)
-        private readonly userRespository: Repository<User>,
-        private readonly jwtService: JwtService,
-    ) {}
-
-    //login method which authenticas a user and assigns jwt token
-    async login(loginDto: LoginDto): Promise<{ userId: string; token: string }> {
-        const { email, password } = loginDto;
-
-    const user = await this.userRespository.findOne({
-        where: {email}
-    });
-
-    if(!user || !(await bcrypt.compare(password, user.passwordHash ?? ' '))) {
-        throw new UnauthorizedException(' Invalid Credentials!')
-    }
-
-    const token = this.jwtService.sign({ 
-        userId: user.id,
-        email: user.email 
-      });
-
-      return {
-        userId: user.id,
-        token,
-      };
-
-}
-
-//verifies if a user exists by ID
-async validateUser(userId: string): Promise<any> {
-    const user = await this.userRespository.findOne({
-        where: { id: userId }
-    });
-
-    if (!user) {
-        throw new UnauthorizedException('User Not Found');
-    }
-
-    return user;
-}
-}
-
-=======
   constructor(
     @InjectRepository(User)
     private readonly userRespository: Repository<User>,
